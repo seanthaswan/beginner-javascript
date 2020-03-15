a4261a5c1b7f17434d8320c644cba88cdfda50aa
Sean Swanson <sean-swan@hotmail.com>
2020-03-14 19:13:00 -0700

    Finished DOM Cardio exercise

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,27 +1,70 @@
 // Make a div
+const myDiv = document.createElement('div');
 
 // add a class of wrapper to it
+myDiv.classList.add('wrapper');
 
 // put it into the body
+const { body } = document;
+body.appendChild(myDiv);
 
 // make an unordered list
+const myUl = document.createElement('ul');
 
 // add three list items with the words "one, two three" in them
+const threeItems = ['one', 'two', 'three'];
+
+const makeThreeItems = () => {
+  let item;
+  for (let i = 0; i < threeItems.length; i += 1) {
+    item = document.createElement('li');
+    item.textContent = threeItems[i];
+    console.log(item);
+    myUl.appendChild(item);
+  }
+};
+
+makeThreeItems();
+
 // put that list into the above wrapper
+myDiv.appendChild(myUl);
 
 // create an image
+const myImg = document.createElement('img');
 
 // set the source to an image
+myImg.src =
+  'https://cdn.dribbble.com/users/2332534/screenshots/6007806/002_alana_tomlin_japanese_dancing_frogs.jpg';
+
 // set the width to 250
+myImg.width = 250;
+
 // add a class of cute
+myImg.classList.add('cute');
+
 // add an alt of Cute Puppy
+myImg.alt = 'Cute Puppy';
+
 // Append that image to the wrapper
+myDiv.appendChild(myImg);
 
 // with HTML string, make a div, with two paragraphs inside of it
-// put this div before the unordered list from above
+// eslint-disable-next-line no-undef
+const createNode = html =>
+  new DOMParser().parseFromString(html, 'text/html').body.firstChild;
+const divFromHtmlString = createNode('<div><p>Ayo!!</p><p>Second P!</p></div>');
+console.log(divFromHtmlString);
 
+// put this div before the unordered list from above
+myUl.insertBefore(divFromHtmlString, myUl.childNodes[0]);
 // add a class to the second paragraph called warning
-// remove the first paragraph
+const secondDiv = document.querySelectorAll('div')[1];
+secondDiv.classList.add('warning');
+
+// remove the first paragraph These both work!!
+
+// 1. document.querySelectorAll('div')[0].remove();
+// /* 2. */ document.querySelector('div:first-of-type').remove();
 
 // create a function called generatePlayerCard that takes in three arguments: name, age, and height
 
@@ -31,14 +74,53 @@
 //   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
 // </div>
 
+const generatePlayerCard = (name, age, height) => {
+  const playerCardHtml = `
+    <div class="playerCard">
+    <h2>${name} - ${age}</h2>
+    <p>They are ${height} and ${age} years old. In Dog years this person would be ${age *
+    7}. That would be a tall dog!</p>
+    <button class="delete">Delete</button>
+    </div>`;
+
+  return createNode(playerCardHtml);
+};
+
+console.log(generatePlayerCard('Sean', 25, '6\'1"'));
+
 // make a new div with a class of cards
-
+const newDiv = document.createElement('div');
+newDiv.classList.add('cards');
+console.log(newDiv);
 // Have that function make 4 cards
-
+const players = [
+  ['Sean', 25, '6\'1"'],
+  ['Jasmine', 43, '5\'6"'],
+  ['Deidra', 29, '5\'9"'],
+];
+players.map(
+  (player, i) =>
+    (players[i] = generatePlayerCard(player[0], player[1], player[2]))
+);
+console.log(players);
 // append those cards to the div
+const appendNodeArrayToDestination = (nodeArray, destEl) => {
+  console.log(destEl);
+  for (let i = 0; i < nodeArray.length; i += 1) {
+    destEl.appendChild(nodeArray[i]);
+  }
+};
+appendNodeArrayToDestination(players, newDiv);
 // put the div into the DOM just before the wrapper element
+myDiv.insertBefore(newDiv, myDiv.childNodes[0]);
 // Bonus, put a delete Button on each card so when you click it, the whole card is removed
-
+const handleDeleteButtonClick = e => {
+  console.log(e.target.parentNode.remove());
+};
+const deleteButtons = document.querySelectorAll('.delete');
+deleteButtons.forEach((button, i) => {
+  button.addEventListener('click', handleDeleteButtonClick);
+});
 // select all the buttons!
 // make out delete function
 // loop over them and attach a listener
